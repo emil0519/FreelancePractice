5046b0a339327a27ad835c61bb1adba881f01d13
Emil Lau <emillau@nine-yi.com>
2023-09-05 18:31:10 +0800

    Add whatsapp link & index.html

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,6 +5,7 @@ import { useGetIp } from "./hooks/useGetIp";
 import Content from "./pages/Content";
 import Translation from "./pages/Translation";
 import "./util/i18n";
+import whatsAppLogo from "./images/whatsapp.png";
 
 function App() {
   const { country, getIPError } = useGetIp();
@@ -33,6 +34,12 @@ function App() {
     <div className="bg-black h-screen flex flex-col gap-9 text-white w-full">
       <Translation />
       <Content />
+      <div
+        className="fixed bottom-5 right-4 w-fit h-fit cursor-pointer"
+        onClick={() => window.open("https://wa.me/8860901055121", "_blank")}
+      >
+        <img src={whatsAppLogo} alt="" className="w-[50px] h-[50px]" />
+      </div>
     </div>
   );
 }
